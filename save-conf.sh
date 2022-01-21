8c9a4314dd34d7e027434f0a01da4ac808ad15ad
xyon3 <reyespauldavid@gmail.com>
2022-01-21 14:12:40 +0800

    fixed saving methods

--- save-conf.sh
+++ save-conf.sh
@@ -1,1 +1,20 @@
-# todo!!
+#!/bin/bash
+
+dHOME="/home/$USER/"
+
+conf_loc=(
+	"${dHOME}.config/i3/"	
+	"${dHOME}.config/alacritty/"
+	"${dHOME}.config/fusuma/"
+	"${dHOME}.config/lf/"
+	"${dHOME}.config/rofi/"
+	"${dHOME}.bashrc"
+	"${dHOME}.vimrc"
+	"/etc/X11/xorg.conf.d/"
+)
+
+for i in {0..7} 
+do
+	cp -r ${conf_loc[i]} ./ 	
+	echo "SAVED: ${conf_loc[i]}"
+done
